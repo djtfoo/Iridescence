0f86b3d95e6093d5cb2ef08525b4af03cf90c4db
sentobi <howcomee@outlook.com>
2017-05-10 01:32:25 +0800

    - added GameProgress static class

---
+++ Assets/Scripts/Generic/GameProgress.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// class to store save data that reflects game's progress
+public static class GameProgress {
+
+    static Cutscene[] cutscenes;
+
+}
+
+enum CUTSCENE_TYPE
+{
+    CS_ANIMATED,
+    CS_FRAMES
+}
+
+struct Cutscene
+{
+    CUTSCENE_TYPE cutscene_type;
+    bool seenBefore;
+    // condition
+}
