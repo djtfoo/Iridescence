c3398069aac28c2853d19a59c34cf407a133e60c
sentobi <howcomee@outlook.com>
2017-05-16 01:23:46 +0800

    - CSVReader static class - generate level from csv file

---
+++ Assets/Scripts/Framework/CSVReader.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+public static class CSVReader {
+
+	public static string[,] GetCSVGridString(string text)
+    {
+        string[] lines = text.Split("\n"[0]);
+
+        // find the rows and columns inside the CSV
+        int rows = lines.Length;    // Y
+        int columns = SplitCSVLine(lines[0]).Length;  // X
+
+        string[,] CSVgrid = new string[rows + 1, columns + 1];
+        for (int y = 0; y < lines.Length; ++y)  // row
+        {
+            string[] line = SplitCSVLine(lines[y]);
+            for (int x = 0; x < line.Length; ++x)   // column
+            {
+                CSVgrid[y,x] = line[x];
+            }
+        }
+
+        return CSVgrid;
+    }
+
+    public static int[,] GetCSVGridInt(string text)
+    {
+        string[] lines = text.Split("\n"[0]);
+
+        // find the rows and columns inside the CSV
+        int rows = lines.Length;    // Y
+        int columns = SplitCSVLine(lines[0]).Length;  // X
+
+        int[,] CSVgrid = new int[rows + 1, columns + 1];
+        for (int y = 0; y < lines.Length; ++y)  // row
+        {
+            string[] line = SplitCSVLine(lines[y]);
+            for (int x = 0; x < line.Length; ++x)   // column
+            {
+                int.TryParse(line[x], out CSVgrid[y, x]);
+            }
+        }
+
+        return CSVgrid;
+    }
+
+    private static string[] SplitCSVLine(string line)
+    {
+        string[] values = Regex.Split(line, ",");
+
+        return values;
+    }
+
+}
---
+++ Assets/Scripts/Game Data/GenerateLevel.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateLevel : MonoBehaviour {
+
+    public TextAsset csvFile;
+    int[,] levelGrid;
+
+    public Transform genericTile;
+    public Sprite[] sprites;
+
+    // Use this for initialization
+    void Start()
+    {
+        // get level grid data
+        levelGrid = CSVReader.GetCSVGridInt(csvFile.text);
+
+        int rows = levelGrid.GetUpperBound(0);
+        int columns = levelGrid.GetUpperBound(1);
+
+        // create terrain
+        // increase X by 0.62, increase Y by 0.36
+        Vector3 startingPos = Vector3.zero;
+        for (int y = 0; y <= rows; ++y)
+        {
+            for (int x = 0; x <= columns; ++x)
+            {
+                if (levelGrid[y, x] == 0)
+                    continue;
+
+                float posX = (0.62f * y) + (0.62f * x);
+                float posY = (-0.36f * y) + (0.36f * x);
+
+                Transform newTile = (Transform)Instantiate(genericTile, new Vector3(0, 0, 0), Quaternion.identity);
+                newTile.SetParent(this.transform);
+                newTile.position = new Vector3(posX, posY, 1f);
+                switch (levelGrid[y,x])
+                {
+                    case 1:
+                        newTile.GetComponent<SpriteRenderer>().sprite = sprites[0];
+                        break;
+                    case 2:
+                        newTile.GetComponent<SpriteRenderer>().sprite = sprites[1];
+                        break;
+                    case 3:
+                        newTile.GetComponent<SpriteRenderer>().sprite = sprites[2];
+                        break;
+                    case 4:
+                        newTile.GetComponent<SpriteRenderer>().sprite = sprites[3];
+                        break;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
